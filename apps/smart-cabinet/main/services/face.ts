b5c57e3248e6c0228a50e1b6286d21f615a0e47a
liqingshan <15054854614@163.com>
2023-12-22 10:22:11 +0800

    fix

--- apps/smart-cabinet/main/services/face.ts
+++ apps/smart-cabinet/main/services/face.ts
@@ -1,10 +1,15 @@
 import { initFaceEngine, initFaceSDK, onlineActivation } from '@smart-cabinet/features'
 import { ipcMain } from 'electron'
-import cv from '@u4/opencv4nodejs'
+
+// import cv from '@u4/opencv4nodejs'
 import { FACE_EVENT_NAME } from '#/ipcNames'
 
+const cv = require('@u4/opencv4nodejs')
+
+let camera = null
+
 function openCamera() {
-  const camera = new cv.VideoCapture(0)
+  camera = new cv.VideoCapture(0)
   setInterval(() => {
     const frame = camera.read()
     if (!frame.empty) {
@@ -12,6 +17,10 @@ function openCamera() {
       console.log('🚀 ~ file: face.ts:14 ~ setInterval ~ frameData:', frameData)
     }
   }, 1000 / 30) // 每秒30帧
+}
+
+export function closeCamera() {
+  camera.release()
 }
 
 function initFace() {
