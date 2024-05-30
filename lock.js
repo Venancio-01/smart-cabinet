75b8fb1e5881e6fefc9e6aca7b433673b24db7b7
liqingshan <15054854614@163.com>
2024-05-30 18:26:46 +0800

    1

--- lock.js
+++ lock.js
@@ -3,18 +3,34 @@ const eventEmitter = require('./emit');
 
 let prevState = 0
 
+function debounce(func, delay) {
+  let timerId;
+  return function() {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+
+const handleCloseDoor = debounce(function(state) {
+  if (prevState == 0 && state == 1) {
+    console.log('pin 23 -' + state)
+    eventEmitter.emit('startRfidReading');
+  }
+
+  prevState = state;
+}, 300);
+
+
 const gpio23 = new Gpio({
   pin: 23, mode: 'in', ready: () => {
     setInterval(function() {
       gpio23.read()
-        .then((state) => {
-          if (prevState == 0 && state == 1) {
-            console.log('pin 23 -' + state)
-            eventEmitter.emit('startRfidReading');
-          }
-
-          prevState = state;
-        });
+        .then(handleCloseDoor);
     }, 200)
   }
 });
