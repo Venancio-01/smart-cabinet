6e7fe5f7ae2765f88828ae2a18d31bca6f98eee7
liqingshan <15054854614@163.com>
2024-05-31 10:20:45 +0800

    1

--- src/lock.js
+++ src/lock.js
@@ -29,19 +29,37 @@ const handleCloseDoor = debounce(function(state) {
   prevState = state;
 }, 300);
 
-let prevState = 0
+let prevState = 0;
+let lastStableState = 0;
+let lastDebounceTime = 0;
+const debounceDelay = 50; // 50ms的去抖动延迟
+
 const gpio19 = new Gpio({
   pin: 19, mode: 'in', ready: () => {
     setInterval(function() {
       gpio19.read()
         .then((state) => {
-          // handleCloseDoor(state)
-          if (prevState == 0 && state == 1) {
-            console.log('pin 19 -' + state)
-            eventEmitter.emit('startRfidReading');
+          const currentTime = Date.now();
+
+          if (state !== lastStableState) {
+            // 如果状态与上次稳定状态不同，则重置去抖动时间
+            lastDebounceTime = currentTime;
           }
 
-          prevState = state;
+          if ((currentTime - lastDebounceTime) > debounceDelay) {
+            // 如果状态已经稳定超过去抖动延迟时间
+            if (lastStableState !== state) {
+              lastStableState = state;
+
+              // 检查从低电平到高电平的变化
+              if (prevState == 0 && state == 1) {
+                console.log('pin 19 -' + state);
+                eventEmitter.emit('startRfidReading');
+              }
+
+              prevState = state;
+            }
+          }
         });
     }, 200)
   }
